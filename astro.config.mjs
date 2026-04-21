9cc43ecbf9b7aa9c76718c0d5f23aaa08bffa5b0
Alex <me@naibuu.dev>
2026-04-21 15:28:40 +0300

    chore: switch to server output (vercel wont stop bitching)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+    output: 'server',
     vite: {
         plugins: [tailwindcss()],
         // build: {
